4bcf30be5e35c8b0ec468086c4bdb6c34a0af6f2
ahami14 <ahnikkahamilton14@gmail.com>
2019-10-15 08:48:12 -0500

    initial file upload

---
+++ Marketing/Marketing/Contestant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketing
+{
+    class Contestant
+    {
+        //member variables
+        string firstName;
+        string lastName;
+        string eMail;
+        string address;
+        int regNumber;
+
+
+        //constructor
+
+        //member methods
+    }
+}
---
+++ Marketing/Marketing/Sweepstakes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketing
+{
+    class Sweepstakes
+    {
+        //member variables
+          
+
+        //constructor
+        public Sweepstakes()
+        {
+
+        }
+
+        //member methods
+        public void SweepstakesRec(string name)
+        {
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
